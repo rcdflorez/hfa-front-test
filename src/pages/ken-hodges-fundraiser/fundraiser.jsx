74984879be78fa984d6d70cb6472bebd882e6aee
Ricardo Florez <ricardo@Ricardos-Mac-mini.local>
2024-01-31 11:05:49 -0500

    fundraising page

--- src/pages/ken-hodges-fundraiser/fundraiser.jsx
+++ src/pages/ken-hodges-fundraiser/fundraiser.jsx
@@ -24,6 +24,16 @@ const FundRaiser = () => {
           data-website-id="pWrTI3EUnv5mzoKhOBszmLgBk7r69Nhwcxt0QuObGEM"
           data-hostname="5b549ac7cacc4d7cb90524683b5c5736.svc.dynamics.com"
         ></div>
+        <script src="https://mktdplp102cdn.azureedge.net/public/latest/js/form-loader.js?v=1.84.2007"></script>{" "}
+        <div
+          className="d365-mkt-config"
+          style={{ display: "none" }}
+          data-website-id="8kcEq0SiFYTDeZfsVLbYBWX5bd0VQ_6xYlGYaFRpOrM"
+          data-hostname="5b549ac7cacc4d7cb90524683b5c5736.svc.dynamics.com"
+        >
+          {" "}
+        </div>{" "}
+        <div data-form-block-id="bd5f3c3c-d9be-ee11-9079-00224805cb79"></div>
       </>
     </>
   );
