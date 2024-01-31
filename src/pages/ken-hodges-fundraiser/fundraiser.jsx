a04d48c69093ceb3361583e461913176a81dfe34
Ricardo Florez <ricardo@Ricardos-Mac-mini.local>
2024-01-31 11:20:14 -0500

    fundraising page

--- src/pages/ken-hodges-fundraiser/fundraiser.jsx
+++ src/pages/ken-hodges-fundraiser/fundraiser.jsx
@@ -3,37 +3,18 @@ const FundRaiser = () => {
     <>
       <p>Test Form</p>
       <>
-        <script src="https://mktdplp102cdn.azureedge.net/public/latest/js/form-loader.js?v=1.84.2007"></script>{" "}
-        <div
-          className="d365-mkt-config"
-          style={{ display: "none" }}
-          data-website-id="8kcEq0SiFYTDeZfsVLbYBWX5bd0VQ_6xYlGYaFRpOrM"
-          data-hostname="5b549ac7cacc4d7cb90524683b5c5736.svc.dynamics.com"
-        >
-          {" "}
-        </div>{" "}
-        <div data-form-block-id="bd5f3c3c-d9be-ee11-9079-00224805cb79"></div>
         {/* */}
-        <div data-form-block-id="907884cd-50c0-ee11-9078-002248044e4d"></div>{" "}
+        <div data-form-block-id="f63e7449-54c0-ee11-9079-002248081fc5"></div>{" "}
         <script src="https://mktdplp102cdn.azureedge.net/public/latest/js/form-loader.js?v=1.84.2007"></script>{" "}
-        <div id="dpWrTI3EUnv5mzoKhOBszmLgBk7r69Nhwcxt0QuObGEM"></div>
+        <div id="dKjQXXLu_2iFpNRTTVvZ351PtHFki-_gq7ARfoJRwv4Y"></div>
         <script src="https://mktdplp102cdn.azureedge.net/public/latest/js/ws-tracking.js?v=1.84.2007"></script>
         <div
           className="d365-mkt-config"
           style={{ display: "none" }}
-          data-website-id="pWrTI3EUnv5mzoKhOBszmLgBk7r69Nhwcxt0QuObGEM"
+          data-website-id="KjQXXLu_2iFpNRTTVvZ351PtHFki-_gq7ARfoJRwv4Y"
           data-hostname="5b549ac7cacc4d7cb90524683b5c5736.svc.dynamics.com"
         ></div>
-        <script src="https://mktdplp102cdn.azureedge.net/public/latest/js/form-loader.js?v=1.84.2007"></script>{" "}
-        <div
-          className="d365-mkt-config"
-          style={{ display: "none" }}
-          data-website-id="8kcEq0SiFYTDeZfsVLbYBWX5bd0VQ_6xYlGYaFRpOrM"
-          data-hostname="5b549ac7cacc4d7cb90524683b5c5736.svc.dynamics.com"
-        >
-          {" "}
-        </div>{" "}
-        <div data-form-block-id="bd5f3c3c-d9be-ee11-9079-00224805cb79"></div>
+        {/* */}
       </>
     </>
   );
