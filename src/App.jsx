ef6f11f787744fa4bf9f8599e4ae7eedfe08edc9
Ricardo Florez <ricardo@Ricardos-Mac-mini.local>
2024-01-31 10:33:21 -0500

    fundraising page

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
-import './App.scss'
-import Layout from './components/Layout/Layout'
-import Home from './pages/Home'
-import NotFound from './pages/NotFound'
-import Team from './pages/Team/Team'
-import Events from './pages/Events/Events'
-import Titan from './pages/Titan/Titan'
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import "./App.scss";
+import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import Team from "./pages/Team/Team";
+import Events from "./pages/Events/Events";
+import Titan from "./pages/Titan/Titan";
+import Foundraising from "./pages/ken-hodges-fundraiser/fundraiser";
 // import { useContextGlobal } from './context/global.context'
 
 function App() {
   // const {state} = useContextGlobal()
-  
+
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path='/' element={<Layout />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path='/team' element={<Team />} />
-        <Route path='/events' element={<Events />} />
-        <Route path='/titan-100' element={<Titan />} />
-        <Route path='*' element={<NotFound />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/titan-100" element={<Titan />} />
+        <Route path="/ken-hodges-fundraiser" element={<Foundraising />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     )
-  )
-  return (
-      <RouterProvider router={router} />
   );
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
