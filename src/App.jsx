aac627d6c3d795a699df4602963b46aaaedabcb5
Ricardo Florez <ricardo@Ricardos-Mac-mini.local>
2024-02-16 12:19:06 -0500

    atlanta 500 implemented

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import NotFound from "./pages/NotFound";
 import Team from "./pages/Team/Team";
 import Events from "./pages/Events/Events";
 import Titan from "./pages/Titan/Titan";
+import Titan500 from "./pages/Titan-500/Titan";
 import Foundraising from "./pages/ken-hodges-fundraiser/fundraiser";
 // import { useContextGlobal } from './context/global.context'
 
@@ -24,6 +25,7 @@ function App() {
         <Route path="/team" element={<Team />} />
         <Route path="/events" element={<Events />} />
         <Route path="/titan-100" element={<Titan />} />
+        <Route path="/altlanta-500" element={<Titan500 />} />
         <Route path="/ken-hodges-fundraiser" element={<Foundraising />} />
         <Route path="*" element={<NotFound />} />
       </Route>
--- src/pages/ken-hodges-fundraiser/fundraiser.jsx
+++ src/pages/ken-hodges-fundraiser/fundraiser.jsx
@@ -4,17 +4,6 @@ const FundRaiser = () => {
       <p>Test Form</p>
       <>
         {/* */}
-        <div data-form-block-id="f63e7449-54c0-ee11-9079-002248081fc5"></div>{" "}
-        <script src="https://mktdplp102cdn.azureedge.net/public/latest/js/form-loader.js?v=1.84.2007"></script>{" "}
-        <div id="dKjQXXLu_2iFpNRTTVvZ351PtHFki-_gq7ARfoJRwv4Y"></div>
-        <script src="https://mktdplp102cdn.azureedge.net/public/latest/js/ws-tracking.js?v=1.84.2007"></script>
-        <div
-          className="d365-mkt-config"
-          style={{ display: "none" }}
-          data-website-id="KjQXXLu_2iFpNRTTVvZ351PtHFki-_gq7ARfoJRwv4Y"
-          data-hostname="5b549ac7cacc4d7cb90524683b5c5736.svc.dynamics.com"
-        ></div>
-        {/* */}
         <script src="https://mktdplp102cdn.azureedge.net/public/latest/js/form-loader.js?v=1.84.2007"></script>{" "}
         <div
           className="d365-mkt-config"
@@ -25,6 +14,7 @@ const FundRaiser = () => {
           {" "}
         </div>{" "}
         <div data-form-block-id="fd639d34-56c0-ee11-9079-002248081fc5"></div>
+        {/* */}
       </>
     </>
   );
