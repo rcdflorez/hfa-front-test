8fa77064d00035aac4bfc18beacc041e09f133a0
Ricardo Florez <ricardo@Ricardos-Mac-mini.local>
2024-02-29 21:43:40 -0500

    updated footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,14 @@
-import './Footer.scss'
-import logo from '../../assets/logo_hive.webp'
-import { Container } from 'react-bootstrap'
-import IconLocation from '../../assets/icons/location-dot-solid.svg'
-import IconLinkedId from '../../assets/icons/linkedin.webp'
+import "./Footer.scss";
+import logo from "../../assets/logo_hive.webp";
+import { Container } from "react-bootstrap";
+import IconLocation from "../../assets/icons/location-dot-solid.svg";
+import IconLinkedId from "../../assets/icons/linkedin.webp";
 
 const Footer = () => {
   return (
     <footer className="footer">
       <Container>
-        <aside className='footer_logo'>
+        <aside className="footer_logo">
           <img src={logo} alt="" />
         </aside>
 
@@ -16,43 +16,50 @@ const Footer = () => {
           <div className="footer_data_social_network">
             <div className="contact location">
               <img src={IconLocation} alt="" />
-              <p>3340 Peachtree Rd NE Suite 1275 Atlanta, Georgia, United States</p>
+              <p>
+                3340 Peachtree Rd NE Suite 1275 Atlanta, Georgia United States
+              </p>
             </div>
             <div className="contact social">
               <a
-                href="https://www.linkedin.com/company/hive-financial-assets/" 
-                target="_blank" 
-                rel="noopener noreferrer">
-                  <img src={IconLinkedId} alt="" />
-                  <p>Hive Financial Assets</p>
+                href="https://www.linkedin.com/company/hive-financial-assets/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={IconLinkedId} alt="" />
+                <p>Hive Financial Assets</p>
               </a>
             </div>
           </div>
-          <form className='footer_data_form'>
-            <input 
-              type="text" 
-              name="form-email" 
-              id="form-email" 
-              placeholder='Your email'/>
-            <input 
-              type="text" 
-              name="form-name" 
-              id="form-name" 
-              placeholder='Your name'/>
+          <form className="footer_data_form">
+            <input
+              type="text"
+              name="form-email"
+              id="form-email"
+              placeholder="Your email"
+            />
+            <input
+              type="text"
+              name="form-name"
+              id="form-name"
+              placeholder="Your name"
+            />
 
-            <textarea 
-              name="form-message" 
-              id="form-message" 
+            <textarea
+              name="form-message"
+              id="form-message"
               rows="5"
-              placeholder='Your message'>
-            </textarea>
+              placeholder="Your message"
+            ></textarea>
             <button>SEND YOUR MESSAGE</button>
           </form>
         </section>
-        <p className='footer_copy'>COPYRIGHT © 2023 HIVE FINANCIAL ASSETS - ALL RIGHTS RESERVED.</p>
+        <p className="footer_copy">
+          COPYRIGHT © 2024 HIVE FINANCIAL ASSETS - ALL RIGHTS RESERVED.
+        </p>
       </Container>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
