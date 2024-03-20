bdefa18abcbb955abdd86f6ad0a046823980e70a
Ricardo Florez <ricardo@Ricardos-Mac-mini.local>
2024-03-20 13:27:29 -0400

    update LK link

--- src/components/About/AboutUs.jsx
+++ src/components/About/AboutUs.jsx
@@ -1,43 +1,44 @@
-import { Container } from 'react-bootstrap'
-import './AboutUs.scss'
-import video1 from '../../assets/images/about/about-us.webp'
-import video2 from '../../assets/images/about/about-us-02.webp'
-import { useEffect, useState } from 'react'
-
+import { Container } from "react-bootstrap";
+import "./AboutUs.scss";
+import video1 from "../../assets/images/about/about-us.webp";
+import video2 from "../../assets/images/about/about-us-02.webp";
+import { useEffect, useState } from "react";
 
 const AboutUs = () => {
-  const [fund, setFund] = useState(false)
-  const [image, setImage] = useState(false)
-  const env = import.meta.env
+  const [fund, setFund] = useState(false);
+  const [image, setImage] = useState(false);
+  const env = import.meta.env;
   useEffect(() => {
-    fetch(
-      `${env.VITE_CMS_BASE_URL}${env.VITE_PAGE__URL}about-the-fund`
-    )
+    fetch(`${env.VITE_CMS_BASE_URL}${env.VITE_PAGE__URL}about-the-fund`)
       .then((response) => {
         return response.json();
       })
       .then((json) => {
         const htmlString = json[0].content.rendered
-          .split('\n')
-          .filter(p => p !== "")
-          .slice(-1)[0]
-          
+          .split("\n")
+          .filter((p) => p !== "")
+          .slice(-1)[0];
+
         const regex = /src="([^"]+)"/;
         const matches = htmlString.match(regex);
         if (matches && matches.length > 1) {
-          const imageUrl = matches[1]
-          setImage(imageUrl)
+          const imageUrl = matches[1];
+          setImage(imageUrl);
         }
         setFund(json[0]);
       });
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
-    <Container fluid className='about-us px-0 mt-5 d-md-flex' id='about-the-found'>
-      {fund && 
+    <Container
+      fluid
+      className="about-us px-0 mt-5 d-md-flex"
+      id="about-the-found"
+    >
+      {fund && (
         <>
           <div className="about-img me-md-5 me-lg-3">
-            <img src={image} alt=""/>
+            <img src={image} alt="" />
           </div>
           <div className="content">
             <div className="content_about d-flex flex-column">
@@ -49,31 +50,38 @@ const AboutUs = () => {
                 }>
               </div> */}
               {fund.content.rendered
-                .split('\n')
+                .split("\n")
                 .filter((p) => p !== "")
                 .slice(0, -1)
-                .map((el, i)=> 
-                  <div key={i}
-                    className='content_about d-flex flex-column'
-                    dangerouslySetInnerHTML={
-                      { __html: el }
-                    }>
-                  </div>
-              )}
+                .map((el, i) => (
+                  <div
+                    key={i}
+                    className="content_about d-flex flex-column"
+                    dangerouslySetInnerHTML={{ __html: el }}
+                  ></div>
+                ))}
             </div>
             <div className="content_videos mt-4 d-flex flex-column flex-lg-row">
-              <a href="https://cactus-4.wistia.com/medias/2z444moozm" target="_blank" rel="noopener noreferrer">
+              <a
+                href="https://hivefa.wistia.com/medias/2qbwqtf33n"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <img src={video1} alt="" />
               </a>
-              <a href="https://cactus-4.wistia.com/medias/0gx2h8e63v" target="_blank" rel="noopener noreferrer">
+              <a
+                href="https://hivefa.wistia.com/medias/wyucowpn3q"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <img src={video2} alt="" />
               </a>
             </div>
           </div>
         </>
-      }
+      )}
     </Container>
-  )
-}
+  );
+};
 
-export default AboutUs
+export default AboutUs;
--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,29 +1,30 @@
-import { Container } from 'react-bootstrap'
-import './Events.scss'
-import { Link } from 'react-router-dom'
+import { Container } from "react-bootstrap";
+import "./Events.scss";
+import { Link } from "react-router-dom";
 
 const Events = () => {
-
   return (
-    <section className='events'>
-      <Container className='d-md-flex justify-content-end'>
+    <section className="events">
+      <Container className="d-md-flex justify-content-end">
         <div className="events_content">
-            <>
-              <p>
-                Join us for the Q1 FY2023 Investor Update to 
-                discuss performance and strategy. 
-                Q&A to follow.
-              </p>
-              <h3>Investor <br />Update</h3>
-              <Link to="/events">
-                <p className='events_code'>Q1 FY 2023</p>
-              </Link>
-              <span></span>
-            </>
+          <>
+            <p>
+              Join us for the Q4 FY2023 Investor Update to discuss performance
+              and strategy. Q&A to follow.
+            </p>
+            <h3>
+              Investor <br />
+              Update
+            </h3>
+            <Link to="https://q4-fy2023-investor-update-full-video.hivefinancialassets.com/">
+              <p className="events_code">Q4 FY 2023</p>
+            </Link>
+            <span></span>
+          </>
         </div>
       </Container>
     </section>
-  )
-}
+  );
+};
 
-export default Events
+export default Events;
